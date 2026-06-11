9c3d807e48785d36bc8971ea7c205b6663bf6f89
Matthew Foster <matthew.foster@hakai.org>
2026-06-10 21:33:22 -0700

    run as script rather then function

--- telegraf/send_http_req_GA.py
+++ telegraf/send_http_req_GA.py
@@ -12,29 +12,25 @@ SENTRY_CRONS = (
     + "/145d7992e3df09af3614048708523550/"
 )
 
-def main():
-    url = os.environ.get(
-        'GA_URL', 'http://www.google-analytics.com/mp/collect')
-    secret = os.environ.get(
-        'GA_API_SECRET')
-    id = os.environ.get(
-        'GA_MEASUREMENT_ID')
+url = os.environ.get(
+    'GA_URL', 'http://www.google-analytics.com/mp/collect')
+secret = os.environ.get(
+    'GA_API_SECRET')
+id = os.environ.get(
+    'GA_MEASUREMENT_ID')
 
-    full_url = f"{url}?api_secret={secret}&measurement_id={id}"
+full_url = f"{url}?api_secret={secret}&measurement_id={id}"
 
-    for line in sys.stdin:
-        sys.stdout.write(line)
-        json_obj = json.loads(line)
-        headers = {
-        # "User-Agent": json_obj['agent'],
-        # "X-Forwarded-For": json_obj['host'],
-        "Content-Type": "application/json"
-        }
-        x = requests.post(full_url, json=json_obj, headers=headers)
-        sys.stdout.write(x.text)
-
-    x = requests.get(SENTRY_CRONS + "?environment=" + os.environ.get('SENTRY_ENVIRONMENT') + "&status=ok")
+for line in sys.stdin:
+    sys.stdout.write(line)
+    json_obj = json.loads(line)
+    headers = {
+    # "User-Agent": json_obj['agent'],
+    # "X-Forwarded-For": json_obj['host'],
+    "Content-Type": "application/json"
+    }
+    x = requests.post(full_url, json=json_obj, headers=headers)
     sys.stdout.write(x.text)
 
-if __name__ == "__main__":
-    main()
+x = requests.get(SENTRY_CRONS + "?environment=" + os.environ.get('SENTRY_ENVIRONMENT') + "&status=ok")
+sys.stdout.write(x.text)
