a1f7529802e5e9bf317dfd5cac4641f6dec9d1a5
Matthew Foster <matthew.foster@hakai.org>
2026-06-10 18:42:19 -0700

    try manual checkins

--- telegraf/send_http_req_GA.py
+++ telegraf/send_http_req_GA.py
@@ -5,7 +5,9 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 import sentry_sdk
-from sentry_sdk.crons import monitor
+# from sentry_sdk.crons import monitor
+from sentry_sdk.crons import capture_checkin
+from sentry_sdk.crons.consts import MonitorStatus
 
 load_dotenv(Path(__file__).parent / ".env", override=False)
 load_dotenv(override=False)
@@ -14,8 +16,12 @@ sentry_sdk.init(
     send_default_pii=True,
 )
 
-@monitor(monitor_slug='hakai-erddap-telegraf-checkin')
+# @monitor(monitor_slug='hakai-erddap-telegraf-checkin')
 def main():
+    check_in_id = capture_checkin(
+        monitor_slug="hakai-erddap-telegraf-checkin",
+        status=MonitorStatus.IN_PROGRESS,
+    )
     url = os.environ.get(
         'GA_URL', 'http://www.google-analytics.com/mp/collect')
     secret = os.environ.get(
@@ -35,7 +41,12 @@ def main():
         }
         x = requests.post(full_url, json=json_obj, headers=headers)
         sys.stdout.write(x.text)
+    capture_checkin(
+        monitor_slug="hakai-erddap-telegraf-checkin",
+        check_in_id=check_in_id,
+        status=MonitorStatus.OK,
+    )
+    sentry_sdk.flush()
 
 if __name__ == "__main__":
     main()
-    sentry_sdk.flush()
