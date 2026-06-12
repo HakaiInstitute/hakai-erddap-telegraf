201bf8160acb12de59217a47fe5e43c8e49aabe3
Matthew Foster <matthew.foster@hakai.org>
2026-06-12 13:23:27 -0700

    move sentry check-in inside for loop which is watching for input. make it run every 5 min or more so we don't spam sentry

--- telegraf/send_http_req_GA.py
+++ telegraf/send_http_req_GA.py
@@ -2,6 +2,7 @@ import sys
 import requests
 import json
 import os
+import time
 
 SENTRY_MONITOR_SLUG = "hakai-erddap-telegraf-checkin"
 SENTRY_INGEST = "https://o56764.ingest.us.sentry.io"
@@ -12,18 +13,6 @@ SENTRY_CRONS = (
     + "/145d7992e3df09af3614048708523550/"
 )
 
-headers = {"Content-Type": "application/json"}
-json_obj = json.loads(
-    '{"environment": "' + os.environ.get("SENTRY_ENVIRONMENT") + '","status": "ok"}'
-)
-x = requests.post(
-    SENTRY_CRONS,
-    json=json_obj,
-    headers=headers,
-)
-sys.stdout.write(x.text)
-
-
 url = os.environ.get(
     'GA_URL', 'http://www.google-analytics.com/mp/collect')
 secret = os.environ.get(
@@ -32,6 +21,9 @@ id = os.environ.get(
     'GA_MEASUREMENT_ID')
 
 full_url = f"{url}?api_secret={secret}&measurement_id={id}"
+
+SENTRY_CHECKIN_INTERVAL = 300  # seconds
+last_sentry_checkin = 0
 
 for line in sys.stdin:
     sys.stdout.write(line)
@@ -43,3 +35,11 @@ for line in sys.stdin:
     }
     x = requests.post(full_url, json=json_obj, headers=headers)
     sys.stdout.write(x.text)
+
+    now = time.monotonic()
+    if now - last_sentry_checkin >= SENTRY_CHECKIN_INTERVAL:
+        sentry_headers = {"Content-Type": "application/json"}
+        sentry_body = {"environment": os.environ.get("SENTRY_ENVIRONMENT"), "status": "ok"}
+        x = requests.post(SENTRY_CRONS, json=sentry_body, headers=sentry_headers)
+        sys.stdout.write(x.text)
+        last_sentry_checkin = now
